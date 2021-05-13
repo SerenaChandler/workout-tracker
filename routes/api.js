42e5dd2c4491033a1a38c1504ead5ee80adcac93
Serena Chandler <serenachandler2019@gmail.com>
2021-05-12 19:18:43 -0700

    added api routes

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const { Workout } = require("../models");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+
+
+router.put("/api/workouts/:id", (req, res) => {
+Workout.findByIdAndUpdate(req.params.id, )
+})
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
+      res.json(dbTransaction);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
